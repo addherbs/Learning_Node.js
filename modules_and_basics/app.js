f956d4ddb427ccd1f5edab3132f7022ac2f96102
Aditya Pandey <addherbs@gmail.com>
2017-09-03 04:29:23 -0500

    working with core module path and using various features

--- modules_and_basics/app.js
+++ modules_and_basics/app.js
@@ -10,6 +10,7 @@ movies.num1();
 movies.num2();
 */
 
+// Implement Object Factory
 /*
 var thisMovies = movies();
 movies.num1 = "Yo my favourite movie is matrix"
@@ -17,8 +18,24 @@ console.log(movies.num1)
 */
 
 
+// Implement core module FS
+/*
 var fs = require('fs');
 
-//fs.writeFileSync("ThisFile.txt", "This is the sentence that we go inside ThisFile.txt");	// File was created
+fs.writeFileSync("ThisFile.txt", "This is the sentence that we go inside ThisFile.txt");	// File was created
 
 console.log(fs.readFileSync("ThisFile.txt").toString());
+*/
+
+
+var path = require('path');
+
+var thisApp = "Desktop/tutorials/nodejs_basics/app.js";
+var thisMovies = "Desktop/tutorials/nodejs_basics/movies.js";
+
+console.log(path.normalize(thisApp));
+console.log(path.normalize(thisMovies));
+console.log(path.dirname(thisMovies));
+console.log(path.basename(thisApp));
+console.log(path.extname(thisMovies));
+
