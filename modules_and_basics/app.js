901cb5e1fb6a1c21bf4ea40063481d3885562fe4
Aditya Pandey <addherbs@gmail.com>
2017-09-03 04:26:05 -0500

    implementing fs module with write function

--- modules_and_basics/app.js
+++ modules_and_basics/app.js
@@ -10,6 +10,13 @@ movies.num1();
 movies.num2();
 */
 
+/*
 var thisMovies = movies();
 movies.num1 = "Yo my favourite movie is matrix"
 console.log(movies.num1)
+*/
+
+
+var fs = require('fs');
+fs.writeFileSync("ThisFile.txt", "This is the sentence that we go inside ThisFile.txt");	// File was created
+
