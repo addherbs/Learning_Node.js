7bd686b9dd545139e045876b3176575a2bc3cad9
Aditya Pandey <addherbs@gmail.com>
2017-09-03 04:25:09 -0500

    implement object factory and using an object for movies.js

--- modules_and_basics/app.js
+++ modules_and_basics/app.js
@@ -5,5 +5,11 @@ movies.callMe();	// Will print
 movies.lol1234;		// wont print
 */
 
+/*
 movies.num1();
 movies.num2();
+*/
+
+var thisMovies = movies();
+movies.num1 = "Yo my favourite movie is matrix"
+console.log(movies.num1)
