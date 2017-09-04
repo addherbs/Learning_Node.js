c805dd828b819db692f177ec8a6356ef63db8c9d
Aditya Pandey <addherbs@gmail.com>
2017-09-04 04:05:09 -0500

    perfected server.js but without error handling

--- modules_and_basics/server.js
+++ modules_and_basics/server.js
@@ -4,3 +4,9 @@ var fs = require('fs');
 http.createServer(onRequest).listen(8000);
 console.log("server has started!")
 
+function onRequest(req, res){
+	console.log(" A user request has been made: " + req.url);
+	res.writeHead(200, { "Context-Type": "text/plain"});
+	res.write("Here is your respnsonse");
+	res.end();
+}
