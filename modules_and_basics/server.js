cd53f0057d9f96e1350de7d6ec6a8079e50b4b1e
Aditya Pandey <addherbs@gmail.com>
2017-09-04 04:06:29 -0500

    Final server.js
    Here we have created our complete server.js where we have implemented a
    server and error handling.
    It gives error if we try to go to anyother map apart from the one
    specified('/')

--- modules_and_basics/server.js
+++ modules_and_basics/server.js
@@ -4,9 +4,33 @@ var fs = require('fs');
 http.createServer(onRequest).listen(8000);
 console.log("server has started!")
 
+//404 response
+function send404Response(res){
+    res.writeHead(404, {"Cotent-Type": "text/plain"});
+    res.write("Error 404 page not found");
+    res.end();
+
+}
+
+// handle the request
+function onRequest(req,res) {
+    /*
+    This if checks if the request if from GET method and only as root call. Any other calls will be given in else
+     */
+    if(req.method == "GET" && req.url == "/"){
+        res.writeHead(200,{"Cotent-Type": "text/html"});
+        fs.createReadStream("./index.html").pipe(res);
+    }else{
+        send404Response(res);
+    }
+}
+
+/*
 function onRequest(req, res){
 	console.log(" A user request has been made: " + req.url);
 	res.writeHead(200, { "Context-Type": "text/plain"});
 	res.write("Here is your respnsonse");
 	res.end();
 }
+
+*/
